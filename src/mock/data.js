8f4d20960ec86d8657a03f96966b3be7ca5a3872
Sydney Stokes <sydneystokes@Sydneys-MacBook-Pro.local>
2021-09-09 18:48:48 -0400

    Added template and customized

--- src/mock/data.js
+++ src/mock/data.js
@@ -2,25 +2,25 @@ import { nanoid } from 'nanoid';
 
 // HEAD DATA
 export const headData = {
-  title: '', // e.g: 'Name | Developer'
-  lang: '', // e.g: en, es, fr, jp
-  description: '', // e.g: Welcome to my website
+  title: 'Amber | Biologist', // e.g: 'Name | Developer'
+  lang: 'en', // e.g: en, es, fr, jp
+  description: 'Welcome to my website',
 };
 
 // HERO DATA
 export const heroData = {
-  title: '',
-  name: '',
-  subtitle: '',
-  cta: '',
+  title: 'Hello my name is, ',
+  name: 'Amber',
+  subtitle: 'I\'m a Biologist',
+  cta: 'Know more',
 };
 
 // ABOUT DATA
 export const aboutData = {
-  img: 'profile.jpg',
-  paragraphOne: '',
-  paragraphTwo: '',
-  paragraphThree: '',
+  img: 'amberPic.png',
+  paragraphOne: 'It\'s me, Amber',
+  paragraphTwo: 'Yooooooo',
+  paragraphThree: 'Wassup',
   resume: 'https://www.resumemaker.online/es.php', // if no resume, the button will not show up
 };
 
@@ -28,30 +28,27 @@ export const aboutData = {
 export const projectsData = [
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    img: 'wise.png',
+    title: 'WISE',
+    info: 'WISE (Women in Science and Engineering) | 2019 - 2020',
+    info2: 'WISE is an innovative, semester-long research and mentoring program at Johns Hopkins University. Here I worked with my mentor, Lin, under the leadership of Dr. Spangler.',
+    url: 'https://home.howard.edu/',
   },
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    img: 'GBA.png',
+    title: 'Gilman Black Alumni Leadership Institute',
+    info: 'Baltimore Leadership Diversity Conference (BSDLC) Planner |2019-2020',
+    info2: 'BSDLC is a high school diversity and equity leadership training event that is planned by and led by area high school students. This year I lead a workshop on Black Mental Health for over 1,000+ students',
+    url: 'https://gbali.org/',
   },
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    img: 'nape.png',
+    title: 'NAPE Conference',
+    info: 'Speaker at the National Alliance for Partnerships and Equity (NAPE) Conference | 2018',
+    info2: 'I was invited to represent the Girl Scouts at the NAPE conference in Crystal City, VA. I spoke at the Youth Voices Luncheon and shared my experiences as a Girl Scout and my interests in STEM.',
+    url: 'https://napequity.org/',
   },
 ];
 
@@ -59,7 +56,7 @@ export const projectsData = [
 export const contactData = {
   cta: '',
   btn: '',
-  email: '',
+  email: 'amber.carter@bison.howard.edu',
 };
 
 // FOOTER DATA
@@ -67,28 +64,18 @@ export const footerData = {
   networks: [
     {
       id: nanoid(),
-      name: 'twitter',
-      url: '',
-    },
-    {
-      id: nanoid(),
-      name: 'codepen',
-      url: '',
+      name: 'instagram',
+      url: 'https://www.instagram.com/amber.carterrr/',
     },
     {
       id: nanoid(),
       name: 'linkedin',
-      url: '',
-    },
-    {
-      id: nanoid(),
-      name: 'github',
-      url: '',
+      url: 'http://www.linkedin.com/in/amberCarter123',
     },
   ],
 };
 
 // Github start/fork buttons
 export const githubButtons = {
-  isEnabled: true, // set to false to disable the GitHub stars/fork buttons
+  isEnabled: false, // set to false to disable the GitHub stars/fork buttons
 };
